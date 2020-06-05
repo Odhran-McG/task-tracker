096e3c06525f4607d7ee0e24d4b5a25a2a9b071f
omcgranaghan <odhran.19@gmail.com>
2020-06-05 12:18:21 +0100

    more commenting

--- client/src/components/EditToDo.js
+++ client/src/components/EditToDo.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from 'react';
 
 const EditTodo = ({todo}) => {
-    const [description, setDescription] = useState(todo.description);
-    console.log(todo);
 
-    //edit description function
+    //Define description states
+    const [description, setDescription] = useState(todo.description);
+    
+    // Edit description function
     const updateDescription = async e => {
         e.preventDefault();
         try {
@@ -17,6 +18,7 @@ const EditTodo = ({todo}) => {
                 body: JSON.stringify(body)
             }
             );
+            // Force refresh
             window.location = "/";
         } catch (error) {
             console.error(error.message);
@@ -25,51 +27,53 @@ const EditTodo = ({todo}) => {
 
     return (
     <Fragment>
-        <button 
-        type="button" 
-        class="btn btn-warning"
-        data-toggle="modal" 
-        data-target={`#id${todo.todo_id}`}
-        >Edit
-        </button> 
+        <button type="button" 
+                class="btn btn-warning"
+                data-toggle="modal" 
+                data-target={`#id${todo.todo_id}`}
+                >Edit
+        </button>
+         
+        <div class="modal fade" 
+             id={`id${todo.todo_id}`}
+             onClick={() => setDescription(todo.description)}>
+        <div class="modal-dialog">
+            
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Edit ToDo</h4>
+                <button type="button" 
+                        class="close" 
+                        data-dismiss="modal"
+                        onClick={() => setDescription(todo.description)}>
+                </button>
+            </div>
 
- <div class="modal fade" 
- id={`id${todo.todo_id}`}
- onClick={() => setDescription(todo.description)}>
-  <div class="modal-dialog">
-    <div class="modal-content">
+            <div class="modal-body">
+                <input type="text" 
+                       className="form-control"
+                       value={description}
+                       onChange={e => setDescription(e.target.value)}>
+                </input>
+            </div>
+            
+            <div class="modal-footer">
+                <button type="button" 
+                        class="btn btn-warning" 
+                        data-dismiss="modal"
+                        onClick = {e => updateDescription(e)}>Edit</button>  
+                <button type="button" 
+                        class="btn btn-danger"
+                        onClick={() => setDescription(todo.description)}  
+                        data-dismiss="modal"
+                        >Close
+                </button>
+            </div>
 
-       <div class="modal-header">
-        <h4 class="modal-title">Edit ToDo</h4>
-        <button 
-        type="button" 
-        class="close" 
-        data-dismiss="modal"
-        onClick={() => setDescription(todo.description)}
-        >&times;</button>
+        </div>
       </div>
-
-      <div class="modal-body">
-          <input 
-          type="text" 
-          className="form-control"
-          value={description}
-          onChange={e => setDescription(e.target.value)}>
-          </input>
-      </div>
-
-      <div class="modal-footer">
-      <button type="button" class="btn btn-warning" data-dismiss="modal"
-      onClick = {e => updateDescription(e)}>Edit</button>  
-        <button type="button" class="btn btn-danger"
-        onClick={() => setDescription(todo.description)} data-dismiss="modal">Close</button>
-      </div>
-
     </div>
-  </div>
-</div>
     </Fragment>
     )};
-
 
 export default EditTodo;
