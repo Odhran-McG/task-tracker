412f57687244a1b849577ad0d56327974716bd28
omcgranaghan <odhran.19@gmail.com>
2020-06-07 21:51:40 +0100

    add test for App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+
+// components
+import InputToDo from "./components/InputToDo";
+import ListToDo from "./components/ListToDo";
+
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render( 
+  <Fragment>
+    <div className="container">
+      <InputToDo/>
+      <ListToDo/>        
+    </div>
+  </Fragment>
+  );
+  expect(App)
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('test react App renders correct markup', ()=> {
+  describe(App, () => {
+    it(App, () => {
+      expect(App.render).equal(
+      <Fragment>
+      <div className="container">
+        <InputToDo/>
+        <ListToDo/>        
+      </div>
+    </Fragment>)
+    });
+  })
 });
