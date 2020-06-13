1b7623eba20fe5803ce400cadda2686e62f1b16a
omcgranaghan <odhran.19@gmail.com>
2020-06-13 22:04:34 +0100

    Import testing components

--- __tests__/state-functions.test.js
+++ __tests__/state-functions.test.js
@@ -1,3 +1,6 @@
+import InputToDo from "./components/InputToDo";
+import EditToDo from "./components/EditToDo";
+
 describe('Addition', () => {
     it('knows that 2 and 2 make 4', () => {
       expect(2 + 2).toBe(4);
